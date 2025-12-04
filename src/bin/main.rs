0bfcde6c393b98e15c9acc80b8745d6a3e9e5817
semanavasco <semanavasco@gmail.com>
2025-12-04 21:59:53 +0100

    Experiment 1: Filling screen with a solid color

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,9 @@
+//! ESPlay Micro v2 - Learning
+//!
+//! Initializes the ILI9341 LCD display and fills it with a solid color.
+//!
+//! Pin mapping: GPIO18 (SCK), GPIO23 (MOSI), GPIO5 (CS), GPIO12 (DC), GPIO2 (RST), GPIO27 (BL)
+
 #![no_std]
 #![no_main]
 #![deny(
@@ -6,34 +12,77 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use esp_hal::clock::CpuClock;
-use esp_hal::main;
-use esp_hal::time::{Duration, Instant};
+use embedded_graphics::pixelcolor::Rgb565;
+use embedded_graphics::prelude::RgbColor;
+use esp_hal::{
+    Blocking,
+    clock::CpuClock,
+    delay::Delay,
+    gpio::{Level, Output, OutputConfig},
+    main, spi,
+    spi::master::{Config, Spi},
+};
+use mipidsi::Builder;
+use mipidsi::interface::SpiInterface;
+use mipidsi::models::ILI9341Rgb565;
+use mipidsi::options::{ColorOrder, Orientation};
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-extern crate alloc;
+const HEAP_SIZE: usize = 98768;
+const DISPLAY_WIDTH: u16 = 320;
+const DISPLAY_HEIGHT: u16 = 240;
 
-// This creates a default app-descriptor required by the esp-idf bootloader.
-// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
 #[main]
 fn main() -> ! {
-    // generator version: 1.0.1
-
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
+    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: HEAP_SIZE);
 
-    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
+    // LCD pins
+    let dc = Output::new(peripherals.GPIO12, Level::High, OutputConfig::default());
+    let cs = Output::new(peripherals.GPIO5, Level::High, OutputConfig::default());
+    let _bl = Output::new(peripherals.GPIO27, Level::High, OutputConfig::default());
+    let mut rst = Output::new(peripherals.GPIO2, Level::Low, OutputConfig::default());
+
+    // SPI setup
+    let spi_bus: Spi<'_, Blocking> = spi::master::Spi::new(peripherals.SPI2, Config::default())
+        .expect("Could not create SPI bus")
+        .with_sck(peripherals.GPIO18)
+        .with_mosi(peripherals.GPIO23);
+    let spi_device = embedded_hal_bus::spi::ExclusiveDevice::new_no_delay(spi_bus, cs)
+        .expect("Could not create SPI device");
+
+    let mut buf = [0u8; 512];
+    let spi_iface = SpiInterface::new(spi_device, dc, &mut buf);
+
+    // Display initialization with hardware reset
+    let mut delay = Delay::new();
+    rst.set_low();
+    delay.delay_millis(20u32);
+    rst.set_high();
+    delay.delay_millis(200u32);
+
+    let mut display = Builder::new(ILI9341Rgb565, spi_iface)
+        .orientation(Orientation::new())
+        .display_size(DISPLAY_HEIGHT, DISPLAY_WIDTH)
+        .color_order(ColorOrder::Bgr)
+        .init(&mut delay)
+        .expect("Could not initialize display");
+
+    // Fill screen with solid color
+    let total_pixels = (DISPLAY_WIDTH as u32) * (DISPLAY_HEIGHT as u32);
+    let pixels = core::iter::repeat(Rgb565::BLUE).take(total_pixels as usize);
+    display
+        .set_pixels(0, 0, DISPLAY_HEIGHT, DISPLAY_WIDTH, pixels)
+        .expect("Could not draw to display");
 
     loop {
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        delay.delay_millis(500u32);
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
 }
