407e378ec03891023acc1bb75238f43f190747ec
semanavasco <semanavasco@gmail.com>
2026-02-11 10:33:03 +0100

    Fresh restart

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{delay::Delay, prelude::*};
+
+extern crate alloc;
+use core::mem::MaybeUninit;
+
+fn init_heap() {
+    const HEAP_SIZE: usize = 32 * 1024;
+    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
+
+    unsafe {
+        esp_alloc::HEAP.add_region(esp_alloc::HeapRegion::new(
+            HEAP.as_mut_ptr() as *mut u8,
+            HEAP_SIZE,
+            esp_alloc::MemoryCapability::Internal.into(),
+        ));
+    }
+}
+
+#[entry]
+fn main() -> ! {
+    #[allow(unused)]
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    let delay = Delay::new();
+    init_heap();
+
+    esp_println::logger::init_logger_from_env();
+
+    loop {
+        log::info!("Hello world!");
+        delay.delay(500.millis());
+    }
+}
